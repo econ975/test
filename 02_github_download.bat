65f16e9de8fba59589abe29d8680b5dc01f9cdde
econ975 <123249528+econ975@users.noreply.github.com>
2024-01-09 18:28:36 +0800

    Update 02_github_download.bat

--- 02_github_download.bat
+++ 02_github_download.bat
@@ -1,6 +1,6 @@
 @echo off
 CHCP 65001
-set /p github_address=enter GitHub repository address：
+set /p github_address=enter GitHub repository address:
 set /p github_branch=enter branch:
 PortableGit\bin\git.exe clone -b %github_branch% %github_address% temp
 echo finish
